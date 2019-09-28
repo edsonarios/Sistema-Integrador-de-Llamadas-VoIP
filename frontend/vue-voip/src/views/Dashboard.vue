b8fce72a25cfcfa5973d26bb1c4f48437032aca2
Daniel <codedany789@gmail.com>
2019-09-28 18:03:46 -0400

    rd add :art:

--- frontend/vue-voip/src/views/Dashboard.vue
+++ frontend/vue-voip/src/views/Dashboard.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="dashboard">
-    <h1>Dashboard</h1>
-		<router-link to="/">Exit</router-link>
+    <h1>Dashboard</h1><router-link to="/">Exit</router-link>
   </div>
 </template>
