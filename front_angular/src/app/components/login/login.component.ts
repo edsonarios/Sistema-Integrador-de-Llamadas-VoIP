12cae92eb25e324ae9fe42aa474a8959638e3e3a
evolutionv51 <jorgeluiscastrov5@gmail.com>
2019-10-12 11:29:54 -0400

    fron-angular

---
+++ front_angular/src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../../sass/main.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
