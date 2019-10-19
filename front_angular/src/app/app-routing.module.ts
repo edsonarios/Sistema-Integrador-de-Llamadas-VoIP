6b115591052f81b715c5c551106840702b23e1d3
Richard <richcori94@gmail.com>
2019-10-19 15:30:01 -0400

    Routes añadidas

--- front_angular/src/app/app-routing.module.ts
+++ front_angular/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+{
+   path: 'Login',
+  component: LoginComponent,
+},
+{
+   path: 'Dasboard',
+  component: DashboardComponent,
+},
+
+{
+      path: '',
+      redirectTo: 'Login',
+      pathMatch: 'full',
+    },
+ {
+      path: '**',
+      redirectTo: 'Login',
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
