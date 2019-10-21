3dd04b958fc679ce52fc174d92afd296a1154c7f
Henrymon342 <https://github.com/henrymon342>
2019-10-21 08:49:31 -0400

    servicios de login, addUsuario, addSala, findAllUser

--- front_angular/src/models/user.ts
+++ front_angular/src/models/user.ts
@@ -1,13 +1,16 @@
-export class User{
+export class User {
     constructor(
         public nombre: string,
-        public apPaterno: string, 
+        public apPaterno: string,
         public apMaterno: string,
         public tipo: string,
         public direccion: string,
         public telefono: string,
         public correo: string,
         public password: string,
-        public conectado: boolean
-    ){}
+        public conectado: boolean,
+        public salaId: string
+    ) { }
+
+
 }
