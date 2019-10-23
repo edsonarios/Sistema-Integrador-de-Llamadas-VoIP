8f5b8c98f9f64aea9f6683ff1042f6771c4a7090
evolutionv5 <44754214+evolutionv5@users.noreply.github.com>
2019-10-23 17:54:13 -0400

    ajustes..

--- front_angular/src/app/app-routing.module.ts
+++ front_angular/src/app/app-routing.module.ts
@@ -13,12 +13,11 @@ const routes: Routes = [
   path: 'Agregar',
   component: AgregarContactosComponent,
 },
-  {
-    path: 'Operador',
-    loadChildren: () => import('./components/operador/operador.module')
-      .then(m => m.OperadorModule),
-  },
-
+{
+  path: 'Operador',
+  loadChildren: () => import('./components/operador/operador.module')
+    .then(m => m.OperadorModule),
+},
 
 {
       path: '',
