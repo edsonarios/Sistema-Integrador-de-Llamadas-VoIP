377060f80be94efb1cbb1700a4d75cda95ce3775
Richard <richcori94@gmail.com>
2019-10-25 04:56:10 -0400

    interaccion de salas y opciones + links dropdown

--- front_angular/src/app/components/templates/operador-template/operador-template.component.ts
+++ front_angular/src/app/components/templates/operador-template/operador-template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../../../models/user';
+import { Sala } from '../../../../models/sala';
 
 @Component({
   selector: 'operador-template',
@@ -7,8 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../../sass/main.scss']
 })
 export class OperadorTemplateComponent implements OnInit {
+user: User;
+public sala;
+
 
   ngOnInit() { 
   	console.log("Carga el dashboard");
+  	this.sala=[{"nombreSala":"dato Prueba",'descripcion':'Descripcion','usuarioId':'1'},
+  	{"nombreSala":"dato Prueba2",'descripcion':'Descripcion2','usuarioId':'2'},
+  	{"nombreSala":"dato Prueba3",'descripcion':'Descripcion3','usuarioId':'3'},];
+
+  	this.user={
+  		'nombre': 'usuario',
+        'apPaterno': 'userPat',
+        'apMaterno': 'userMat',
+        'tipo': 'Operador',
+        'direccion': 'Prueba',
+        'telefono': '12345',
+        'correo': 'ope@operador',
+        'password': '1234',
+        'conectado': true,
+        'salaId': '1'
+  	};
+  	//console.log(this.sala);
+  	//console.log(this.user);
   }
+  datosSala(id,nombre,descripcion){
+  	 window.alert('Id : '+id+"\n Nombre : "+nombre+'\n Descripcion : '+descripcion+'\n \n \t Listo para recibir metodos!');
+  }
+   DialPad(){
+  	 window.alert('pendiente');
+  }
+  settings(){
+  	 window.alert('pendiente plox');
+  }
+	addSala(){
+  	 window.alert('pendiente , nueva ventana + ruta');
+  }
+
 }
