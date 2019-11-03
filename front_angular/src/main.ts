6d5a68f9d2ff349aa037af3f3dca8bee78db8cee
Daniel <codedany789@gmail.com>
2019-11-03 15:20:57 -0400

    añadida la funcionalidad HMR al proyecto :art:

--- front_angular/src/main.ts
+++ front_angular/src/main.ts
@@ -2,11 +2,20 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
 	enableProdMode();
 }
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.catch(err => console.error(err));
+if (environment.hmr) {
+	if (module['hot']) {
+		hmrBootstrap(module, bootstrap);
+	} else {
+		console.error('HMR is not enabled for dev-server!');
+		console.log('Are you using the --hmr flag for ng serve?');
+	}
+} else {
+	bootstrap().catch(err => console.log(err));
+}
