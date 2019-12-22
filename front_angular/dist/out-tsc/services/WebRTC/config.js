f17e7e83483eee70f6bf9cd177f22af084ee738a
Richardson94 <richcori94@gmail.com>
2019-12-21 21:08:10 -0400

    Remove directory dist

--- front_angular/dist/out-tsc/services/WebRTC/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    WS: "wss://localhost:8089/ws",
-    // WS: "wss://192.168.1.100:8089/ws",
-    HOST: "localhost"
-    // HOST: "192.168.1.100"
-};
