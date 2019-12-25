4607847c70b651d790eb924b96a7d991f71a1a78
edsonarios <edsonrios9@gmail.com>
2019-12-25 15:58:57 -0400

    Añadido cabezeras de api para peticiones GET POST PUT DELETE

--- mod-api/api.js
+++ mod-api/api.js
@@ -50,9 +50,12 @@ api.use('*', async (req, res, next) => {
 
 
   }
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next()
+  
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-Key, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 })
 
 api.get('/datosPrueba', async (req, res) => {
