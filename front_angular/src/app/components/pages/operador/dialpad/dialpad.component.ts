23089241327aa27c8720fd4ae493bde5d0ce46a1
Daniel Miranda <codedany789@gmail.com>
2019-12-27 02:49:52 -0400

    llamar a sip implementada, colgar llamada

--- front_angular/src/app/components/pages/operador/dialpad/dialpad.component.ts
+++ front_angular/src/app/components/pages/operador/dialpad/dialpad.component.ts
@@ -2,24 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
+import { WebRTCService } from '@services/WebRTC/WebRTC.service';
+import { RTCSession } from 'jssip';
+
 @Component({
 	selector: 'dialpad',
 	templateUrl: './dialpad.component.html'
 })
 export class DialPadComponent implements OnInit {
 	dialNumber: string = '';
-	constructor(private router: Router, private modalService: BsModalService) {
+	constructor(private router: Router, private modalService: BsModalService) {}
 
-	}
-
-	ngOnInit() {}
-	DialNum(Num){
-		this.dialNumber=this.dialNumber+Num;
+	DialNum(Num) {
+		this.dialNumber = this.dialNumber + Num;
 	}
 	Llamada() {
-		window.alert('Llamando al : '+this.dialNumber);
+		window.alert('Llamando al : ' + this.dialNumber);
+		this.sipCall(this.dialNumber);
 	}
-	Limpiar(){
-		this.dialNumber='';
+	Limpiar() {
+		this.dialNumber = '';
+	}
+
+	alert: any;
+	session: WebRTCService;
+	event: RTCSession;
+	call: any;
+	arr: Array<RTCSession> = [];
+	ngOnInit() {
+		this.session = new WebRTCService();
+		this.session.sessionEvents();
+		this.event = this.session.newRTCSession();
+		this.alert = this.session.getMessage();
+		this.arr.push(this.event);
+	}
+	conectar() {
+		this.session.connect();
+		this.alert = this.session.getMessage();
+	}
+	desconectar() {
+		this.session.disconnect();
+		this.alert = this.session.getMessage();
+	}
+
+	sipCall(sip) {
+		this.session.sipCall(sip);
+	}
+	endCall() {
+		this.session.terminate();
 	}
 }
--- front_angular/src/services/WebRTC/config.ts
+++ front_angular/src/services/WebRTC/config.ts
@@ -1,4 +1,5 @@
 export const config = {
-  // HOST: "192.168.1.100"
-  HOST: "localhost"
+	// HOST: '18.216.202.26'
+	HOST: '192.168.1.100'
+	// HOST: "localhost"
 };
