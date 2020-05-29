000aaf71a0b31c922c977c64795922c8a4688ceb
Daniel Miranda <codedany789@gmail.com>
2020-05-29 16:02:08 -0400

    :fire: añadidos campos adicionales en los modelos sip, crd

--- mod-db/models/sip.js
+++ mod-db/models/sip.js
@@ -1,158 +1,193 @@
-'use strict'
+"use strict";
 
-const Sequelize = require('sequelize')
-const setupDatabase = require('../lib/db')
+const Sequelize = require("sequelize");
+const setupDatabase = require("../lib/db");
 
-module.exports = function setupSipModel (config) {
-  const sequelize = setupDatabase(config)
+module.exports = function setupSipModel(config) {
+  const sequelize = setupDatabase(config);
 
-  return sequelize.define('sip', {
+  return sequelize.define("sip", {
     name: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     secret: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     callerid: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     type: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     context: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     host: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
-    disallow:{
+    disallow: {
       type: Sequelize.STRING,
-      allowNull: true
-    }, 
-    allow:{
+      allowNull: true,
+    },
+    allow: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     qualify: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     nat: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     switchsip: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     //Nuevos
     qualifyfreq: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     deny: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dtnfnode: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     canreinvite: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     trustrpid: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     sendrpid: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     transport: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     avpf: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     force_avp: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     icesupport: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     encryption: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     callgroup: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     pickupgroup: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dial: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     permit: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     callcounter: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     faxdetect: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     directmedia: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dtlsenable: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dtlsverify: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dtlscertfile: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dtlscafile: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     dtlssetup: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     rtcp_mux: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
     },
     insecure: {
       type: Sequelize.STRING,
-      allowNull: true
-    }
-    
-    
-  })
-}
+      allowNull: true,
+    },
+    /* ----------------------- Campos adicionales */
+    ipaddr: {
+      type: Sequelize.STRING(45),
+      allowNull: true,
+    },
+    port: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    regseconds: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    defaultuser: {
+      type: Sequelize.STRING(40),
+      allowNull: true,
+    },
+    fullcontact: {
+      type: Sequelize.STRING(80),
+      allowNull: true,
+    },
+    regserver: {
+      type: Sequelize.STRING(20),
+      allowNull: true,
+    },
+    useragent: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    lastms: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    callbackextension: {
+      type: Sequelize.STRING(40),
+      allowNull: true,
+    },
+  });
+};
