46576f7f9f3dd29a916367d95b21c31c3a9299bc
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-06-26 10:29:33 -0400

    añadiendo produccion

--- Asterisk-VoIp-WebRTC-Angular/src/environments/environment.prod.ts
+++ Asterisk-VoIp-WebRTC-Angular/src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
 export const environment = {
 	production: true,
-	hmr: false
+	hmr: false,
+	firebaseConfig: {
+		apiKey: 'AIzaSyCYIMqN5TQDkqFY3Ztg-zQ70d0N1Pp6iio',
+		authDomain: 'ionictracking-a88a5.firebaseapp.com',
+		databaseURL: 'https://ionictracking-a88a5.firebaseio.com',
+		projectId: 'ionictracking-a88a5',
+		storageBucket: 'ionictracking-a88a5.appspot.com',
+		messagingSenderId: '1067702087137',
+		appId: '1:1067702087137:web:358adf3c7beec0dbc636a0',
+		measurementId: 'G-W8KSFDXWBN'
+	},
+	mapboxKey: 'pk.eyJ1IjoiZXZvbHV0aW9udjUiLCJhIjoiY2tibDliaTJvMTZ4eTJxbnpnazI1N3JhMyJ9.eiXflPcOdFpt436gJ9yPZQ',
+	
 };
