4f8f3b3286979cad25cecf53a1ea01155ae65086
evolutionv5 <44754214+evolutionv5@users.noreply.github.com>
2020-08-04 23:25:05 -0400

    trazado de ruta y seguimiento enlazado a la Base de Datos

--- Asterisk-VoIp-WebRTC-Angular/src/services/Firebase/tracking.service.ts
+++ Asterisk-VoIp-WebRTC-Angular/src/services/Firebase/tracking.service.ts
@@ -1,38 +1,59 @@
 import { Injectable } from '@angular/core';
-import {
-	AngularFirestoreCollection,
-	AngularFirestore,
-	AngularFirestoreModule
-} from 'angularfire2/firestore';
+import { AngularFirestoreCollection, AngularFirestore, AngularFirestoreModule } from 'angularfire2/firestore';
 import { TrackingI } from '@administrador/tracking/tracking.interface';
-import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { GLOBAL } from '@services/global';
+import { retry, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root'
 })
 export class TrackingService {
-	private Tracking: AngularFirestoreCollection<TrackingI>;
-	private arrayTrack: Observable<TrackingI[]>;
-	constructor(db: AngularFirestore) {
-		this.Tracking = db.collection<TrackingI>('positions');
-		this.arrayTrack = this.Tracking.snapshotChanges().pipe(
-			map((actions) => {
-				return actions.map((a) => {
-					const data = a.payload.doc.data();
-					const id = a.payload.doc.id;
-					return { id, ...data };
-				});
-			})
-		);
-	}
-	getAll() {
-		return this.arrayTrack;
-	}
-	getById(id: string) {
-		return this.Tracking.doc<TrackingI>(id).valueChanges();
-	}
-	removeById(id: string) {
-		return this.Tracking.doc(id).delete();
-	}
+    private Tracking: AngularFirestoreCollection<TrackingI>;
+    private arrayTrack: Observable<TrackingI[]>;
+    public url: string;
+    constructor(db: AngularFirestore, private http: HttpClient) {
+        this.url = GLOBAL.url;
+        this.Tracking = db.collection<TrackingI>('positions');
+        this.arrayTrack = this.Tracking.snapshotChanges().pipe(
+            map((actions) => {
+                return actions.map((a) => {
+                    const data = a.payload.doc.data();
+                    const id = a.payload.doc.id;
+                    return { id, ...data };
+                });
+            })
+        );
+    }
+
+    errorHandl(error) {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // Get client-side error
+            errorMessage = error.error.message;
+        } else {
+            // Get server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.log(errorMessage);
+        return throwError(errorMessage);
+    }
+    getAll() {
+        return this.arrayTrack;
+    }
+    getById(id: string) {
+        return this.Tracking.doc<TrackingI>(id).valueChanges();
+    }
+    removeById(id: string) {
+        return this.Tracking.doc(id).delete();
+    }
+
+    geoTracking(): Observable<mapboxgl.LngLatLike> {
+        return this.http.get<mapboxgl.LngLatLike>(this.url + 'flespiParse').pipe(retry(1), catchError(this.errorHandl));
+    }
+    geoNumber(): Observable<number[][]> {
+        return this.http.get<number[][]>(this.url + 'flespiParse').pipe(retry(1), catchError(this.errorHandl));
+    }
 }
