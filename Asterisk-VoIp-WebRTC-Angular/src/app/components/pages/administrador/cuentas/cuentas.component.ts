96a549ddbbecd72bc76c07deda92f917bc77e745
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-08-19 16:35:48 -0400

    ajuste de indentacion

--- Asterisk-VoIp-WebRTC-Angular/src/app/components/pages/administrador/cuentas/cuentas.component.ts
+++ Asterisk-VoIp-WebRTC-Angular/src/app/components/pages/administrador/cuentas/cuentas.component.ts
@@ -1,39 +1,30 @@
-import { Component, OnInit ,TemplateRef} from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
-
 
 import { UserService } from '@services/user.service';
 import { User } from '@models/user';
 @Component({
-	selector: 'cuentas',
-	templateUrl: './cuentas.component.html',
-	providers: [UserService],
+    selector: 'cuentas',
+    templateUrl: './cuentas.component.html',
+    providers: [UserService]
 })
 export class CuentasComponent implements OnInit {
-	
-	contactos: [User];
-	
-	constructor(private router: Router,
-              private userservice: UserService
-  ) {
-	 
+    contactos: [User];
 
-	}
+    constructor(private router: Router, private userservice: UserService) {}
 
-	ngOnInit() {this.listarContacto()}
- 
-	
-  listarContacto(){
-		 this.userservice.findAllUsuario()
-		.subscribe(
-		rt => {
-			console.log('Estos son los contactos existentes... \n');
-			this.contactos = rt;
-			console.log(this.contactos);
-		},
-		er => console.log(er)
-		);
-		
-  }	
-  
+    ngOnInit() {
+        this.listarContacto();
+    }
+
+    listarContacto() {
+        this.userservice.findAllUsuario().subscribe(
+            (rt) => {
+                console.log('Estos son los contactos existentes... \n');
+                this.contactos = rt;
+                console.log(this.contactos);
+            },
+            (er) => console.log(er)
+        );
+    }
 }
