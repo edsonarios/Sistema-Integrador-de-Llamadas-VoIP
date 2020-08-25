bde0ec23ed89baa880842c4cf31542ac7186ed8e
alfredo09 <valenciaalfredo09@gmail.com>
2020-08-25 17:46:54 -0400

    Añadida api que cambia el contexto de un sip y la salaId del usuario, dueño del sip

--- mod-db/lib/sip.js
+++ mod-db/lib/sip.js
@@ -1,95 +1,116 @@
-'use strict'
+"use strict";
 
 module.exports = function setupSip(SipModel, UsuarioModel) {
-
   async function create(id, obj) {
     const res = await UsuarioModel.findOne({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
 
     if (res) {
-      Object.assign(obj, { usuarioId: res.id })
-      const result = await SipModel.create(obj)
-      return result.toJSON()
+      Object.assign(obj, { usuarioId: res.id });
+      const result = await SipModel.create(obj);
+      return result.toJSON();
     }
   }
-  
+
   async function createRadio(obj) {
-    const result = await SipModel.create(obj)
-    return result.toJSON()
+    const result = await SipModel.create(obj);
+    return result.toJSON();
   }
 
   async function update(id, obj) {
     const cond = {
       where: {
-        id
-      }
-    }
+        id,
+      },
+    };
 
-    const updated = await SipModel.update(obj, cond)
-    return updated
+    const updated = await SipModel.update(obj, cond);
+    return updated;
   }
 
+  async function updateForNumber(name, obj) {
+    const cond = {
+      where: {
+        name,
+      },
+    };
+
+    const updated = await SipModel.update(obj, cond);
+    return updated;
+  }
   async function updatesipCont(context, obj) {
     const cond = {
       where: {
-        context
-      }
-    }
-    const updated = await SipModel.update(obj, cond)
-    return updated
+        context,
+      },
+    };
+    const updated = await SipModel.update(obj, cond);
+    return updated;
+  }
+
+  async function updatesipCont1(context) {
+    const cond = {
+      where: {
+        context,
+      },
+    };
+    const updated = await SipModel.update(cond);
+    return updated;
   }
 
   async function findById(id) {
     return await SipModel.findOne({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
   }
   async function findAll() {
-    return SipModel.findAll()
+    return SipModel.findAll();
   }
 
   async function findLastSip(query) {
-    return SipModel.findOne(query)
+    return SipModel.findOne(query);
   }
 
   async function destroyAll(id) {
     return await SipModel.destroy({
       where: {
-        salaId: id
-      }
-    })
+        salaId: id,
+      },
+    });
   }
 
   async function destroy(usuarioId) {
     return await SipModel.destroy({
       where: {
-        usuarioId
-      }
-    })
+        usuarioId,
+      },
+    });
   }
 
   async function destroy1(id) {
     return await SipModel.destroy({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
   }
   return {
     create,
     createRadio,
     update,
     updatesipCont,
+    updatesipCont1,
+    updateForNumber,
     findById,
     findLastSip,
     findAll,
     destroyAll,
     destroy,
-    destroy1
-  }
-}
+    destroy1,
+  };
+};
